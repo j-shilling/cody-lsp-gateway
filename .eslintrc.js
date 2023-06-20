9637b7f1b36956806d6a35b649dbd7108f22e7b8
Jake Shilling <shilling.jake@gmail.com>
2023-06-19 20:10:36 -0400

    Add linting, build, and publish workflows

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,37 @@
 // @ts-check
 
-const baseConfig = require('../../.eslintrc')
 module.exports = {
-  extends: '../../.eslintrc.js',
+  extends: '@sourcegraph/eslint-config',
   parserOptions: {
-    ...baseConfig.parserOptions,
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    EXPERIMENTAL_useSourceOfProjectReferenceRedirect: true,
     project: [__dirname + '/tsconfig.json'],
   },
-  overrides: baseConfig.overrides,
+  overrides: [
+    {
+      files: ['*.d.ts'],
+      rules: {
+        'no-restricted-imports': 'off',
+      },
+    },
+    {
+      files: '*.story.tsx',
+      rules: {
+        'react/forbid-dom-props': 'off',
+        'import/no-default-export': 'off',
+      },
+    },
+    {
+      files: ['**/gulpfile.js', '**/story/**.tsx', '**/story/**.ts', '*.story.tsx'],
+      rules: {
+        'no-console': 'off',
+      },
+    },
+  ],
   rules: {
     'id-length': 'off',
     'no-console': 'off',
@@ -18,6 +42,7 @@ module.exports = {
       },
     ],
     'unicorn/filename-case': 'off',
+    'unicorn/expiring-todo-comments': 'off',
     'arrow-body-style': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
   },
